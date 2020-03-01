6d124b1b9ad33f505bd7ef9cfd2cf172580ddac0
Mehdi <azzi_57130@hotmail.fr>
2020-03-01 18:27:52 +0100

    Ajout de la pagination pour les Articles

--- api/Controllers/CatalogueController.cs
+++ api/Controllers/CatalogueController.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace Catalogue.Controllers
 {
@@ -19,35 +20,44 @@ namespace Catalogue.Controllers
         }
 
         [HttpGet]
-        public List<Article> Get()
+        public async Task<ActionResult<IEnumerable<Article>>> GetPost(string page)
         {
             //string[] name = { "Paroi de douche à l'italienne", "Paroi de douche fixe", "Paroi de douche latérale", "Paroi de douche avec LED", "Verre de bureau", "Verre pivotant", "Verrière d'intérieur" };
             //string[] Caract = { "Associé au bois de chêne 400mx600m", "Verre d'intérieur 600mx600m", "Type atelier en aluminium" };
             //string[] detail = { "Parfait pour l'intérieur, solide et épais", "Verre trempé", "Verre de cristal" };
             //Random random = new Random();
 
-            for (int i = 0; i <= 100; i++)
+            //for (int i = 0; i <= 100; i++)
+            //{
+            //    //_context.Article.Add(new Article
+            //    //{
+            //    //    IdCategory = random.Next(1, 4),
+            //    //    IdTag = random.Next(1, 5),
+            //    //    PhotoUrl = "Images/Photo" + i,
+            //    //    Status = random.Next(1, 4).ToString(),
+            //    //    Price = random.Next(15, 400),
+            //    //    Characteristic = Caract[random.Next(0, Caract.Length)],
+            //    //    Detail = detail[random.Next(0, detail.Length)],
+            //    //    Name = name[random.Next(0, name.Length)],
+            //    //});
+            //    //_context.SaveChanges();
+            //}
+            //return articles.Skip(index).Take(10).ToList();
+
+            if (string.IsNullOrEmpty(page))
             {
-                //_context.Article.Add(new Article
-                //{
-                //    IdCategory = random.Next(1, 4),
-                //    IdTag = random.Next(1, 5),
-                //    PhotoUrl = "Images/Photo" + i,
-                //    Status = random.Next(1, 4).ToString(),
-                //    Price = random.Next(15, 400),
-                //    Characteristic = Caract[random.Next(0, Caract.Length)],
-                //    Detail = detail[random.Next(0, detail.Length)],
-                //    Name = name[random.Next(0, name.Length)],
-                //});
-                //_context.SaveChanges();
+                return await _context.Set<Article>().Skip(0).Take(10).ToListAsync();
             }
-            return _context.Article.ToList();
+            else
+            {
+                return await _context.Set<Article>().Skip(int.Parse(page) * 10).Take(10).ToListAsync();
+            }
         }
 
-        [HttpGet("Category/{idCategory}/Article", Name = "GetArticlesByCategory")]
-        public List<Article> GetArticlesByCategory(int idCategory)
+        [HttpGet("NombrePage", Name = "GetNombrePage")]
+        public int GetNombrePage()
         {
-            return _context.Article.Where(x => x.IdCategory == idCategory).ToList();
+            return (int)Math.Ceiling((double)_context.Article.ToList().Count() / 10);
         }
 
         [HttpGet("{id}", Name = "GetArticleById")]
